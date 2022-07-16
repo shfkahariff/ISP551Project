99a7fdf94498d10e076ad150f8c1d861d9dfad28
User <User@LAPTOP-FJ4GSBGU>
2022-07-17 02:32:48 +0800

    try userdb9

--- ISP551DonationProject/src/main/java/donation/db/ConnectionManager.java
+++ ISP551DonationProject/src/main/java/donation/db/ConnectionManager.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class ConnectionManager {
 	static Connection con;
 	private static final String DB_DRIVER="org.postgresql.Driver";
-	private static final String DB_CONNECTION="jdbc:postgres://ec2-44-196-174-238.compute-1.amazonaws.com:5432/d3t8dfkls27nvs";
+	private static final String DB_CONNECTION="jdbc:postgresql://ec2-44-196-174-238.compute-1.amazonaws.com:5432/d3t8dfkls27nvs";
 	private static final String DB_USER="hlrbheqbczvkck";
 	private static final String DB_PASSWORD="fb1bb3afb74dce1181912d32f2115b7380b1eee6081e20073d4de3d522f6e8de";
 	
